100204ef9594c191726d84151f7688cce78b93f7
Ariane Parolin <ariane.calarga@gmail.comp>
2023-03-13 20:31:57 -0300

    Acionando a segunda tela somente

--- duastelas/app/src/main/java/com/example/duastelas/MainActivity.kt
+++ duastelas/app/src/main/java/com/example/duastelas/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.duastelas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnGoTotela2: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // diz qual a janela que vai começar
+
+        btnGoTotela2 = findViewById(R.id.btnGoToTela1)
+        btnGoTotela2.setOnClickListener{
+            val intent = Intent(this, tela2::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
+
